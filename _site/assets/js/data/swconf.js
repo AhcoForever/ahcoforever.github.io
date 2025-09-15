34a3d90f24f98d4b37e720d72f4292a6316c7731
kmy <ahco8766@gmail.com>
2025-09-15 16:07:24 +0900

    addPolicy4

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1757913235',resources: [
+    cacheName: 'chirpy-1757920031',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
