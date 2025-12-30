6b1f630d649899fee1baf5af7b65b7a9875bb8ac
kmy <ahco8766@gmail.com>
2025-12-30 16:41:06 +0900

    imageChanged2

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1757920031',resources: [
+    cacheName: 'chirpy-1767079940',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
