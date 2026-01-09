618ad3d33f76993efcae8631b3677fb47f77843b
miny <ahco8766@kakao.com>
2026-01-09 18:23:27 +0900

    html-test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767079940',resources: [
+    cacheName: 'chirpy-1767950484',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
